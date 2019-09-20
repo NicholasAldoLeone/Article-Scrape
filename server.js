302785519260bc11d0eb55b2c63e7e274a245999
Nicholas Leone <nick.al.leone@gmail.com>
2019-09-20 14:27:02 -0700

    Updating port

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var routes = require("./routes");
 var exphbs = require("express-handlebars");
 var app = express();
 
+var PORT = process.env.PORT || 8080;
+
 mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/mongo_scraper",
     {
@@ -22,6 +24,6 @@ app.set("view engine", "handlebars");
 
 app.use(routes);
 
-app.listen(8080, function(){
+app.listen(PORT, function(){
     console.log("App running on http://localhost:8080");
 })
